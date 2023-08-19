a91afd106505a20e91e9ff89047cd79a986bc855
Maovin <ogunbiyimiracle@gmail.com>
2023-08-18 21:11:55 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_munbers - print numbers as specify
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+	_putchar(a + '0');
+
+}
+_putchar('\n');
+}
