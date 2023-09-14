735e415ddb3031f6fb4043ff5aff01a5797815e7
Maovin <ogunbiyimiracle@gmail.com>
2023-09-14 09:19:02 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: th string to be pinted.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s)
+	}
+}
